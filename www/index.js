0d18a00259d1ca2dc14cb4f3f5660dd3a350058d
sheng yeong <chia.shengyeong@gmail.com>
2022-11-10 17:25:17 +0800

    webassembly

--- www/index.js
+++ www/index.js
@@ -1,14 +1,30 @@
 
 
+
 async function init() {
+  const memory = new WebAssembly.Memory({initial: 1})
+  const importObject = {
+    js: {
+      mem: memory
+    },
+    console: {
+      log: () => {
+        console.log("Just logging something!");
+      },
+      error: () => {
+        console.log("I am just error");
+      }
+    }
+  }
 
   const response = await fetch("sum.wasm");
   const buffer = await response.arrayBuffer();
-  const wasm = await WebAssembly.instantiate(buffer);
 
-  const sumFunction = wasm.instance.exports.sum;
-  const result = sumFunction(100, 300);
-  console.log(result);
+  const wasm = await WebAssembly.instantiate(buffer, importObject);
+  const uint8Array = new Uint8Array(memory.buffer, 0, 2);
+
+  const hiText = new TextDecoder().decode(uint8Array);
+  console.log(hiText);
 }
 
 init();
