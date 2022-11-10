2b0067661a7b145b9019346b8770e437aa886395
sheng yeong <chia.shengyeong@gmail.com>
2022-11-10 17:09:38 +0800

    webpack dev server with plugin

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,10 +1,19 @@
-const path = require("path");
 
- module.exports = {
-   entry: "./index.js",
-   output: {
-     path: path.resolve(__dirname, "public"),
-     filename: "index.js"
-   },
-   mode: "development"
- }
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "index.js"
+  },
+  mode: "development",
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./index.html", to: "./"}
+      ]
+    })
+  ]
+}
